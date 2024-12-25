74ebadc784351980d7a4354be6d63fff51d6f287
krmkarakoc <keremkarakoc@ibaysoft.com.tr>
2024-12-25 16:07:36 +0300

    last

--- src/main/java/com/kkarakoc/config/SecurityConfig.java
+++ src/main/java/com/kkarakoc/config/SecurityConfig.java
@@ -14,35 +14,33 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.kkarakoc.jwt.JwtAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity	
+@EnableWebSecurity
 public class SecurityConfig {
-	
+
 	private static final String AUTHENTICATE = "/authenticate";
 	private static final String REGISTER = "/register";
 	private static final String REFRESH_TOKEN = "/refreshToken";
-	
+	private static final String[] SWAGGER_PATHS = { "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html" };
+
 	@Autowired
 	private AuthenticationProvider authenticationProvider;
-	
+
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
-	
+
 	@Autowired
 	private AuthenticationEntryPoint authenticationEntryPoint;
-	
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable()
-		.authorizeHttpRequests(request-> 
-		request.requestMatchers(AUTHENTICATE,REGISTER,REFRESH_TOKEN)
-		.permitAll()
-		.anyRequest()
-		.authenticated()).exceptionHandling().authenticationEntryPoint(authenticationEntryPoint).and()
-		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-		.authenticationProvider(authenticationProvider)
-		.addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class);
-		
-		
+				.authorizeHttpRequests(request -> request.requestMatchers(AUTHENTICATE, REGISTER, REFRESH_TOKEN)
+						.permitAll().requestMatchers(SWAGGER_PATHS).permitAll().anyRequest().authenticated())
+				.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint).and()
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authenticationProvider(authenticationProvider)
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
 		return http.build();
 	}
 
